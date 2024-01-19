ee69cfccbc3a797b85694a0cf5fc8eeaf8ed86d8
xavaya <xavaya96@gmail.com>
2024-01-19 00:02:07 -0500

    Actualización proyecto Xavaya
    Fecha: 19/01/2024
    Hora: 00:02
    Autor: Xavaya
    
    Resumen:  Cambios significativos en Login Registro, verificaciones obb y demas de auth.

--- assets/dist/settings/auth/login-validation.js
+++ assets/dist/settings/auth/login-validation.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Obtener referencias a los elementos del formulario y los mensajes de error
   const form = document.getElementById('loginForm');
   const emailInput = document.getElementById('emailLogin');
@@ -7,15 +7,29 @@ document.addEventListener('DOMContentLoaded', function() {
   const errorPassword = document.getElementById('errorPassword');
 
   if (form && emailInput && passwordInput) {
-    // Agregar event listeners para validar en tiempo real mientras se escribe
-    emailInput.addEventListener('input', validateEmail);
-    passwordInput.addEventListener('input', validatePassword);
-
     // Agregar event listener para manejar el envío del formulario
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', function (event) {
       if (!validateForm()) {
         event.preventDefault(); // Evita el envío si hay errores
       }
+    });
+
+    // Agregar event listeners para restablecer los errores al cambiar el valor del campo
+    emailInput.addEventListener('input', function () {
+      setTimeout(validateEmail, 0); // Retrasar la validación
+    });
+
+    passwordInput.addEventListener('input', function () {
+      setTimeout(validatePassword, 0); // Retrasar la validación
+    });
+
+    // Agregar event listeners para manejar cambios en el autocompletado del navegador
+    emailInput.addEventListener('change', function () {
+      validateEmail();
+    });
+
+    passwordInput.addEventListener('change', function () {
+      validatePassword();
     });
 
     // Validar el formulario una vez al cargar la página
@@ -27,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const email = emailInput.value.trim();
     const isValidFormat = validateEmailFormat(email);
 
-    setValidity(emailInput, isValidFormat, errorEmail, 'El formato del correo electrónico es inválido');
+    setValidity(emailInput, isValidFormat, errorEmail, 'Asegúrate de que el formato del correo sea correcto.');
   }
 
   // Función para validar la longitud y el formato de la contraseña
@@ -35,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const password = passwordInput.value.trim();
     const isValidFormat = validatePasswordFormat(password);
 
-    setValidity(passwordInput, isValidFormat, errorPassword, 'La contraseña debe tener al menos 6 caracteres');
+    setValidity(passwordInput, isValidFormat, errorPassword, 'La contraseña debe ser de al menos 6 caracteres.');
   }
 
   // Función para establecer la validez y mostrar mensajes de error
@@ -55,12 +69,13 @@ document.addEventListener('DOMContentLoaded', function() {
     return emailRegex.test(email);
   }
 
-  // Función para validar la longitud mínima y el formato de la contraseña
-  function validatePasswordFormat(password) {
-    // Puedes ajustar esta expresión regular según tus requisitos
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
-    return passwordRegex.test(password);
-  }
+// Función para validar la longitud mínima y el formato de la contraseña
+function validatePasswordFormat(password) {
+  // Modifiqué la expresión regular para hacerla menos exigente
+  const passwordRegex = /^[a-zA-Z\d\W_]{6,}$/;
+  return passwordRegex.test(password);
+}
+
 
   // Función principal para validar todo el formulario
   function validateForm() {
--- assets/dist/settings/auth/signup-validation.js
+++ assets/dist/settings/auth/signup-validation.js
@@ -1,64 +1,48 @@
-// Espera a que se cargue el contenido del documento HTML
 document.addEventListener('DOMContentLoaded', () => {
-  // Obtener referencias a los elementos del formulario y los mensajes de error
   const form = document.getElementById('registerForm');
   const emailInput = document.getElementById('emailRegister');
   const passwordInput = document.getElementById('passwordRegister');
-  const emailError = document.getElementById('errorEmail'); // Corregir el ID del emailError
+  const emailError = document.getElementById('errorEmail');
   const passwordError = document.getElementById('errorPasswordRegister');
+  const generalError = document.getElementById('generalError');
 
-  // Agregar event listeners para validar en tiempo real mientras se escribe
-  emailInput.addEventListener('input', validateEmail);
-  passwordInput.addEventListener('input', validatePassword);
+  emailInput.addEventListener('input', validateInput.bind(null, emailInput, emailError, validateEmailFormat, enablePasswordInput, disablePasswordInput));
+  passwordInput.addEventListener('input', validateInput.bind(null, passwordInput, passwordError, validatePasswordFormat));
 
-  // Agregar event listener para manejar el envío del formulario
   form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Evita el envío por defecto
-    validateForm(); // Llama a la función principal de validación
+    event.preventDefault();
+    validateForm();
   });
 
-  // Validar el formulario una vez al cargar la página
-  validateForm();
+  function validateInput(inputElement, errorElement, validationFunction, enableFunction, disableFunction) {
+    const value = inputElement.value.trim();
 
-  // Función para validar el formato del correo electrónico
-  function validateEmail() {
-    const email = emailInput.value.trim();
-
-    // Verificar si el campo está vacío antes de realizar la validación
-    if (email === '') {
-      setValidity(emailInput, emailError, false, 'Por favor, ingrese un correo electrónico');
+    if (value === '') {
+      setValidity(inputElement, errorElement, false, `Por favor, ingrese ${inputElement === emailInput ? 'un correo electrónico' : 'una contraseña'}`);
       return;
     }
 
-    const isValidFormat = validateEmailFormat(email);
+    const isValidFormat = validationFunction(value);
 
-    setValidity(emailInput, emailError, isValidFormat, 'El formato del correo electrónico es inválido');
+    setValidity(inputElement, errorElement, isValidFormat, inputElement === emailInput ? 'El formato del correo electrónico es inválido' : 'La contraseña no cumple con los requisitos');
 
-    // Si el correo es válido, notificar sobre la contraseña
-    if (isValidFormat) {
-      passwordInput.removeAttribute('disabled');
-    } else {
-      passwordInput.setAttribute('disabled', 'disabled');
-      passwordInput.value = ''; // Limpiar el campo de contraseña si el correo no es válido
+    if (isValidFormat && inputElement === emailInput) {
+      enableFunction();
+    } else if (!isValidFormat && inputElement === emailInput) {
+      disableFunction();
     }
   }
 
-  // Función para validar el formato de la contraseña
-  function validatePassword() {
-    const password = passwordInput.value.trim();
-
-    // Verificar si el campo está vacío antes de realizar la validación
-    if (password === '') {
-      setValidity(passwordInput, passwordError, false, 'Por favor, ingrese una contraseña');
-      return;
-    }
-
-    const isValidFormat = validatePasswordFormat(password);
-
-    setValidity(passwordInput, passwordError, isValidFormat, 'La contraseña no cumple con los requisitos');
+  function enablePasswordInput() {
+    passwordInput.removeAttribute('disabled');
   }
 
-  // Función para establecer la validez y mostrar mensajes de error
+  function disablePasswordInput() {
+    passwordInput.setAttribute('disabled', 'disabled');
+    passwordInput.value = '';
+    setValidity(passwordInput, passwordError, false, 'El formato del correo electrónico es inválido');
+  }
+
   function setValidity(inputElement, errorElement, isValid, errorMessage) {
     if (isValid) {
       inputElement.classList.remove('is-invalid');
@@ -69,33 +53,27 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // Función para validar el formato del correo electrónico
   function validateEmailFormat(email) {
-    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
 
-  // Función para validar el formato de la contraseña
   function validatePasswordFormat(password) {
-    // Requisitos: más de 8 caracteres, al menos una minúscula, una mayúscula y un carácter especial
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]{9,}$/;
+    const passwordRegex = /^[a-zA-Z\d\W_]{6,}$/;
     return passwordRegex.test(password);
   }
 
-  // Función principal para validar todo el formulario
   function validateForm() {
-    validateEmail();
-    validatePassword();
+    validateInput(emailInput, emailError, validateEmailFormat, enablePasswordInput, disablePasswordInput);
+    validateInput(passwordInput, passwordError, validatePasswordFormat);
 
     const invalidInputs = document.querySelectorAll('.is-invalid');
 
     if (invalidInputs.length === 0) {
-      // No hay errores, puedes enviar el formulario aquí
       console.log('Formulario válido. Enviando...');
-      form.submit(); // Enviar el formulario si no hay errores
+      form.submit();
     } else {
-      // No mostrar mensajes de error generales en caso de campos inválidos
-      // alert('Por favor, corrija los errores antes de enviar el formulario.');
+      generalError.textContent = 'Por favor, corrija los errores antes de enviar el formulario.';
     }
   }
 });
